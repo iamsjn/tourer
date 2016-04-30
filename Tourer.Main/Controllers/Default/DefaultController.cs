fe3317cce2336ae4a846c142fe0605af027c265f
Khairul Islam <ahmed.sojun@hotmail.com>
2016-04-30 22:16:30 +0600

    Test Commit.

---
+++ Tourer.Main/Controllers/Default/DefaultController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tourer.Model;
+using Tourer.DataAccess;
+using Newtonsoft.Json.Linq;
+
+namespace Tourer.Main
+{
+    public class DefaultController : ApiController
+    {
+        User _user = null;
+        ICollection<int> _iDs = null;
+        IEnumerable<dynamic> _seasonaltouristAttractions = null;
+        IEnumerable<dynamic> _locationaltouristAttractions = null;
+        IEnumerable<dynamic> _touristAttractions = null;
+        JObject _data = null;
+
+        [HttpGet]
+        public IHttpActionResult GetSeasonalTouristAttraction()
+        {
+            TAttractionSeasonInfoActivity oTSeasonActivity = new TAttractionSeasonInfoActivity();
+            TouristAttractionActivity oTouristAttractionActivity = new TouristAttractionActivity();
+            _iDs = new List<int>();
+            _seasonaltouristAttractions = new List<dynamic>();
+            //Here we are changing current datetime's year according to our db stored value
+            var currentDate = DateTime.Today;
+            var modifiedDate = new DateTime(2016, currentDate.Month, currentDate.Day);
+            var seasonDate = new DateTime(2016, currentDate.Month, currentDate.Day);
+
+            if ((modifiedDate >= DateTime.Parse("2016/01/01") && modifiedDate <= DateTime.Parse("2016/03/31")) || (modifiedDate >= DateTime.Parse("2016/11/01") && modifiedDate <= DateTime.Parse("2016/12/31")))
+                _iDs = oTSeasonActivity.GetTAIDs(EnumSeason.Winter);
+
+            if ((modifiedDate >= DateTime.Parse("2016/04/01") && modifiedDate <= DateTime.Parse("2016/07/31")))
+                _iDs = oTSeasonActivity.GetTAIDs(EnumSeason.Summer);
+
+            if ((modifiedDate >= DateTime.Parse("2016/08/01") && modifiedDate <= DateTime.Parse("2016/10/31")))
+                _iDs = oTSeasonActivity.GetTAIDs(EnumSeason.Rainy);
+
+            _seasonaltouristAttractions = oTouristAttractionActivity.GetTouristAttractions(_iDs);
+
+            _data = CommonHelper.GenerateSeasonalTAJObject(_seasonaltouristAttractions);
+
+            return Ok(_data);
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetLocationalTouristAttraction()
+        {
+            TouristAttractionActivity oTouristAttractionActivity = new TouristAttractionActivity();
+            _locationaltouristAttractions = new List<dynamic>();
+
+            _locationaltouristAttractions = oTouristAttractionActivity.GetTouristAttractions();
+
+            _data = CommonHelper.GenerateLocationalTAJObject(_locationaltouristAttractions);
+
+            return Ok(_data);
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetSeasonalAndLocationalTouristAttraction()
+        {
+            TouristAttractionActivity oTouristAttractionActivity = new TouristAttractionActivity();
+            TAttractionSeasonInfoActivity oTSeasonActivity = new TAttractionSeasonInfoActivity();
+            _seasonaltouristAttractions = new List<dynamic>();
+            _iDs = new List<int>();
+            _locationaltouristAttractions = new List<dynamic>();
+            _touristAttractions = new List<dynamic>();
+            //Here we are changing current datetime's year according to our db stored value
+            var currentDate = DateTime.Today;
+            var modifiedDate = new DateTime(2016, currentDate.Month, currentDate.Day);
+            var seasonDate = new DateTime(2016, currentDate.Month, currentDate.Day);
+
+            if ((modifiedDate >= DateTime.Parse("2016/01/01") && modifiedDate <= DateTime.Parse("2016/03/31")) || (modifiedDate >= DateTime.Parse("2016/11/01") && modifiedDate <= DateTime.Parse("2016/12/31")))
+                _iDs = oTSeasonActivity.GetTAIDs(EnumSeason.Winter);
+
+            if ((modifiedDate >= DateTime.Parse("2016/04/01") && modifiedDate <= DateTime.Parse("2016/07/31")))
+                _iDs = oTSeasonActivity.GetTAIDs(EnumSeason.Summer);
+
+            if ((modifiedDate >= DateTime.Parse("2016/08/01") && modifiedDate <= DateTime.Parse("2016/10/31")))
+                _iDs = oTSeasonActivity.GetTAIDs(EnumSeason.Rainy);
+
+            _seasonaltouristAttractions = oTouristAttractionActivity.GetTouristAttractions(_iDs);
+
+            _locationaltouristAttractions = oTouristAttractionActivity.GetTouristAttractions();
+
+            _touristAttractions = _locationaltouristAttractions.Concat(_seasonaltouristAttractions).ToList<dynamic>();
+
+            _data = CommonHelper.GenerateLocationalTAJObject(_touristAttractions);
+
+            return Ok(_data);
+        }
+
+
+
+        [HttpPost]
+        public IHttpActionResult GetReviewedTAttraction(int userID)
+        {
+            TAReviewActivity oTAReviewActivity = new TAReviewActivity();
+            _touristAttractions = new List<dynamic>();
+
+            _touristAttractions = oTAReviewActivity.GetReviewedTAttraction(userID);
+
+            _data = CommonHelper.GenerateReviewedTAJObject(_touristAttractions);
+
+            return Ok(_data);
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult SignUp(UserAndGCM oUserAndGCM)
+        {
+            UserActivity oUserActivity = new UserActivity();
+            GCMActivity oGCMActivity = new GCMActivity();
+            _user = new User();
+            _user = oUserActivity.Save(oUserAndGCM.User);
+
+            if (_user != null)
+            {
+                if (oGCMActivity.Save(oUserAndGCM.GCM, _user.UserID))
+                {
+                    _data = CommonHelper.GenerateUserJObject(_user);
+                    return Ok(_data);
+                }
+                else
+                {
+                    _data = CommonHelper.GenerateCommonJObject("Request Invalid", "invalid");
+                    return Ok(_data);
+                }
+            }
+            else
+            {
+                _data = CommonHelper.GenerateCommonJObject("Request Invalid", "invalid");
+                return Ok(_data);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult SignIn(UserAndGCM oUserAndGCM)
+        {
+            UserActivity oUserActivity = new UserActivity();
+            GCMActivity oGCMActivity = new GCMActivity();
+            _user = new User();
+
+            _user = oUserActivity.IsExist(oUserAndGCM.User);
+
+            if (_user != null)
+            {
+                if (oGCMActivity.Save(oUserAndGCM.GCM, _user.UserID))
+                {
+                    _data = CommonHelper.GenerateUserJObject(_user);
+                    return Ok(_data);
+                }
+                else
+                {
+                    _data = CommonHelper.GenerateCommonJObject("Invalid Email or Password", "invalid");
+                    return Ok(_data);
+                }
+            }
+            else
+            {
+                _data = CommonHelper.GenerateCommonJObject("Invalid Email or Password", "invalid");
+                return Ok(_data);
+            }
+        }
+    }
+}
---
+++ Tourer.Main/Controllers/Detail/DetailController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tourer.DataAccess;
+using Newtonsoft.Json.Linq;
+using Tourer.Model;
+
+namespace Tourer.Main.Controllers
+{
+    public class DetailController : ApiController
+    {
+        IEnumerable<dynamic> _taSuggestions = null;
+        IEnumerable<dynamic> _taPhotos = null;
+        ICollection<dynamic> _reviews = null;
+        ICollection<dynamic> _childReviews = null;
+        ICollection<int> _IDs = null;
+        dynamic _touristAttraction = null;
+        JObject _data = null;
+        double _avgRating = 0.0;
+        TAReview _otAReview = null;
+
+        [HttpPost]
+        public IHttpActionResult GetDetail(int touristAttractionID)
+        {
+            TouristAttractionActivity oTouristAttractionActivity = new TouristAttractionActivity();
+            TAReviewActivity oTAReviewActivity = new TAReviewActivity();
+            TAPhotoActivity oTAPhoto = new TAPhotoActivity();
+            _taSuggestions = new List<dynamic>();
+            _taPhotos = new List<dynamic>();
+            
+
+            _touristAttraction = oTouristAttractionActivity.GetTouristAttraction(touristAttractionID);
+            _avgRating = oTAReviewActivity.GetAverageRating(touristAttractionID);
+            _taPhotos = oTAPhoto.GetTAPhotos(touristAttractionID);
+            _taSuggestions = oTouristAttractionActivity.GetTASuggestions(touristAttractionID);
+            _reviews = oTAReviewActivity.GetTAReviews(touristAttractionID);
+            _IDs = oTAReviewActivity.GetTAReviewIDs(touristAttractionID);
+            _childReviews = oTAReviewActivity.GetTAReviews(_IDs);
+
+            if(_taPhotos == null)
+                _taPhotos = new List<dynamic> { new { Photo = string.Empty } };
+
+            if (_taSuggestions == null)
+                _taSuggestions = new List<dynamic> { new { ID = string.Empty, Name = string.Empty, Photo = string.Empty, Type = string.Empty } };
+
+            if (_reviews == null)
+                _reviews = new List<dynamic> { new { ReviewID = string.Empty, Rating = string.Empty, Review = string.Empty, ReviewDate = string.Empty, UserID = string.Empty, UserName = string.Empty, UserPhoto = string.Empty } };
+
+            if (_childReviews == null)
+                _childReviews = new List<dynamic> { new { ParentID = string.Empty, ReviewID = string.Empty, Review = string.Empty, ReviewDate = string.Empty, UserID = string.Empty, UserName = string.Empty, UserPhoto = string.Empty } };
+
+            _data = CommonHelper.GenerateTADetailJObject(_touristAttraction, _avgRating, _taPhotos, _taSuggestions, _reviews, _childReviews);
+
+            return Ok(_data);
+        }
+
+        public IHttpActionResult PostReview(TAReview oTAReview)
+        {
+            _otAReview = new TAReview();
+            TAReviewActivity oTAReviewActivity = new TAReviewActivity();
+
+            _otAReview.UserID = oTAReview.UserID;
+            _otAReview.TouristAttractionID = oTAReview.TouristAttractionID;
+            _otAReview.Rating = oTAReview.Rating;
+            _otAReview.Review = oTAReview.Review != null ? oTAReview.Review : string.Empty;
+
+            _otAReview = oTAReviewActivity.Save(_otAReview);
+
+            return Ok(_data);
+        }
+    }
+}
